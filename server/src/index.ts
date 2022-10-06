77b4a1f7ed3d537319ff18bd6e13918834e25430
Andrew Bwogi <andrew.bwogi@gmail.com>
2022-10-05 17:39:57 -0700

    work on graphql schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express'; // typescript specific
 import { ApolloServer } from 'apollo-server-express';
+import { schema } from './graphql';
 
-const app = express();
 const port = 9000;
 
 // lets create a new apollo server instance
-const server = new ApolloServer();
+const server = new ApolloServer({ schema });
+
+const app = express();
+await server.start();
 server.applyMiddleware({ app, path: '/api' }); // this describes the endpoint where our graphql endpoint shall live
 
 app.listen(port);
